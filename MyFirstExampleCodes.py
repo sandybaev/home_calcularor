74365901abcce00f7f340e4d01c77061caa03fdb
rauan <sandybaev@mail.ru>
2023-09-15 11:33:29 +0600

    все удалено код начат сначала

--- MyFirstExampleCodes.py
+++ MyFirstExampleCodes.py
@@ -1,29 +1,6 @@
-import math
-
-PI = math.pi
-def radius():
-    n = float(input("Введите диаметр в см: "))
-    n /= 2
-    return n
-
-def height():
-    m = float(input("Введите высоту в см: "))
-    return m
-
-def volume():
-    r = radius()
-    h = height()
-    s = PI*r**2
-    v = s*h
-
-    return v
-
-#print("Объем цилиндра:", volume(), " см3")
-
-def massa(g):
-    n = float(input("Удельный вес в г/см3: ")) #тут я оказывается мало что понимаю в удельных весах
-    return g*n/1000
-
-print("Вес цииндра в кг: ", massa(volume()))
+print('Привет мир!')
 
 
+a = input('введите число, положительное желательно: ')
+
+print(a)
